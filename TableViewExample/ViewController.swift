e42972d87a7ad3ffe375f107be84c3a0d3cee431
Alex <alex@alexsykes.net>
2020-02-15 20:06:42 +0000

    Update

--- TableViewExample/ViewController.swift
+++ TableViewExample/ViewController.swift
@@ -8,16 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let swiftBlogs = ["Alex","Sykes","BL6 5QD","07910765467", "16, Avonhead Close","alex@alexsykes.net"]
+    let textCellIdentifier = "TextCell"
+    
     @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftBlogs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath)
+        
+        let row = indexPath.row
+        cell.textLabel?.text = swiftBlogs[row]
+        return cell
+    }
 }
 
